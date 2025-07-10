fe9ca67af35cda53dd91474cfa16a61eba3d5bc1
Yalçın Yağız KORKMAZ <yykorkmaz20@gmail.com>
2025-07-10 16:16:54 +0300

    prisma

--- eventease/src/lib/prisma.ts
+++ eventease/src/lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+if (process.env.NODE_ENV !== "production") global.prisma = prisma
